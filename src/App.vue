ed4266a362c023267127583adf3f8e83a19bf5e7
snvoke <snvoke@gmail.com>
2017-08-21 08:55:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{message}}</p>
+</template>
+
+<script>
+  // import Greeting from './Greeting.vue'
+
+  export default {
+    data () {
+      return {
+        message: 'Hello'
+      }
+    }
+    // components: {
+    //   Greeting
+    // }
+  }
+</script>
+
+
